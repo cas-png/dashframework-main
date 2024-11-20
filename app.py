7bf8146317dc1c8f07c0484973e6703069e36ed5
cas-png <72526687+cas-png@users.noreply.github.com>
2024-11-20 18:27:33 +0100

    added first visualization
    heatmap visualizations of sharks

--- app.py
+++ app.py
@@ -1,58 +1,40 @@
 from jbi100_app.main import app
 from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
-
-from dash import html
+from dash import html, dcc, Dash
 import plotly.express as px
 from dash.dependencies import Input, Output
+import pandas as pd
 
+from jbi100_app.data import get_data
 
+df = get_data()
+
+# Create density map figure
+fig = px.density_mapbox(
+    df,
+    lat='Latitude',
+    lon='Longitude',
+    radius=5,
+    center=dict(lat=0, lon=180),
+    zoom=3,
+    mapbox_style="open-street-map"
+)
+
+# Initialize the Dash app
+app = Dash()
+
+# Define the layout
+app.layout = html.Div(
+    style={"height": "100vh", "width": "100vw", "margin": 0, "padding": 0},  # Full-screen container
+    children=[
+        dcc.Graph(
+            figure=fig,
+            style={"height": "100%", "width": "100%"}  # Full-screen graph
+        )
+    ]
+)
+
+# Run the server
 if __name__ == '__main__':
-    # Create data
-    df = px.data.iris()
-
-    # Instantiate custom views
-    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df)
-    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
-
-    app.layout = html.Div(
-        id="app-container",
-        children=[
-            # Left column
-            html.Div(
-                id="left-column",
-                className="three columns",
-                children=make_menu_layout()
-            ),
-
-            # Right column
-            html.Div(
-                id="right-column",
-                className="nine columns",
-                children=[
-                    scatterplot1,
-                    scatterplot2
-                ],
-            ),
-        ],
-    )
-
-    # Define interactions
-    @app.callback(
-        Output(scatterplot1.html_id, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        Input(scatterplot2.html_id, 'selectedData')
-    ])
-    def update_scatter_1(selected_color, selected_data):
-        return scatterplot1.update(selected_color, selected_data)
-
-    @app.callback(
-        Output(scatterplot2.html_id, "figure"), [
-        Input("select-color-scatter-2", "value"),
-        Input(scatterplot1.html_id, 'selectedData')
-    ])
-    def update_scatter_2(selected_color, selected_data):
-        return scatterplot2.update(selected_color, selected_data)
-
-
-    app.run_server(debug=False, dev_tools_ui=False)
+    app.run_server(debug=True, use_reloader=False)
--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,10 +1,8 @@
 import plotly.express as px
+import pandas as pd
 
 
 def get_data():
-    # Read data
-    df = px.data.iris()
-
+    df= pd.read_excel('./jbi100_app/dataset/data.xlsx', index_col=0)
     # Any further data preprocessing can go her
-
     return df
